fb64e64d2d3da6f4b882db780fb4b252de7168ca
John Doe <736365465@qq.com>
2018-10-17 11:02:17 +0800

    纵横中文网

---
+++ src/main/java/com/clawhub/minibooksearch/spider/eggs/Zongheng.java
@@ -0,0 +1,12 @@
+package com.clawhub.minibooksearch.spider.eggs;
+
+/**
+ * <Description>纵横中文网<br>
+ *
+ * @author DaShi<br>
+ * @version 1.0<br>
+ * @taskId <br>
+ * @CreateDate 2018/10/17 11:01 <br>
+ */
+public class Zongheng {
+}
